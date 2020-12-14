6e5a85be6fbe5cac3917060c8ccb05e2f3ed1608
jqiuyin <943571574@qq.com>
2020-12-14 21:38:28 +0800

    add mode

--- main_descriptive_stats.py
+++ main_descriptive_stats.py
@@ -1,7 +1,21 @@
 from collections import Counter
+from playStats.descriptive_stats import frequency
+from playStats.descriptive_stats import mode
 
 if __name__ == '__main__':
-    data = [2, 2, 2,2 ,1,1,1,3,3]
+
+    # test conuter
+    data = [2, 2, 2, 2, 1, 1, 1, 3, 3]
     counter = Counter(data)
     print(counter.most_common())
     print(counter.most_common()[0][1])
+
+    # test frequency
+    freq = frequency(data)
+    print(freq)
+
+    # test mode
+    data = [2, 2, 2, 1, 1, 1, 3, 3]
+    mode_elements, mode_count = mode(data)
+    print(mode_elements)
+    print(mode_count)
--- playStats/descriptive_stats.py
+++ playStats/descriptive_stats.py
@@ -8,3 +8,19 @@ def frequency(data):
     for point in counter.most_common():
         ret.append((point[0], point[1]/len(data)))
     return ret
+
+
+def mode(data):
+    """众数"""
+    counter = Counter(data)
+    if counter.most_common()[0][1] == 1:
+        return None, None
+
+    count = counter.most_common()[0][1]
+    ret = []
+    for point in counter.most_common():
+        if point[1] == count:
+            ret.append(point[0])
+        else:
+            break
+    return ret, count
