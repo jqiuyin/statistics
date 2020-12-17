5739e7fec1a2f04a2a3de99837c24cb407a84a09
jqiuyin <943571574@qq.com>
2020-12-17 21:52:07 +0800

    add bar plot  histogram plot

--- main_plot.py
+++ main_plot.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import random
+from collections import Counter
 
 if __name__ == '__main__':
 
@@ -13,4 +14,17 @@ if __name__ == '__main__':
     # line plot
     x = [random.randint(0, 100) for _ in range(100)]
     plt.plot([i for i in range(100)], x)
+    plt.show()
+
+    # bar plot
+    data = [3, 3, 4, 1, 5, 4, 2, 1, 5, 4, 4, 4, 5, 3, 2, 1, 4, 5, 5]
+    counter = Counter(data)
+    x = [point[0] for point in counter.most_common()]
+    y = [point[1] for point in counter.most_common()]
+    plt.bar(x, y)
+    plt.show()
+
+    # histogram
+    data = [random.randint(0, 100) for _ in range(1000)]
+    plt.hist(data, rwidth=0.8, bins=10, density=True)
     plt.show()
